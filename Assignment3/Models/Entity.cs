98b7ea858ebe42b2e7b5070980eae2a4b92bb015
Amanda Venier <amandav6527@gmail.com>
2023-03-03 15:45:09 -0500

    Added Entity class.

---
+++ Assignment3/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Entity
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+    }
+}
