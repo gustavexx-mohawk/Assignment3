3d542bc5532fb2e4862320149207119a8b6796c2
MARC-HI\marcanog <gustavo-marcano-valero@mohawkcollege.ca>
2023-03-04 10:17:06 -0500

    Created Organization class model

---
+++ Assignment3/Models/Organization.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Organization
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
