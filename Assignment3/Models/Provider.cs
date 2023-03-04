04e333a19f61cb8a4095dada6cdcea7638e35143
Samuel Kim <ghassad87@gmail.com>
2023-03-04 17:33:58 -0500

    Modified. inherited to Person class

--- Assignment3/Models/Provider.cs
+++ Assignment3/Models/Provider.cs
@@ -2,18 +2,11 @@
 
 namespace Assignment3.Models
 {
-    public class Provider
+    public class Provider:Person
     {
-        [Key]
-        public Guid Id { get; set; }
-        [Required]
-        public DateTimeOffset CreationTime { get; set; }
-        [Required, StringLength(128)]
-        public string FirstName { get; set; }
-        [Required, StringLength(128)]
-        public string LastName { get; set; }
         [Required]
         public int LicenseNumber { get; set; }
+
         [Required]
         public string Address { get; set; }
     }
