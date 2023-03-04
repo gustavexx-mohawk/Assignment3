189dd8906a24c51bed67a6966361fdc11a1e662b
Samuel Kim <ghassad87@gmail.com>
2023-03-04 17:27:54 -0500

    Created Entity class retrived from Amanda

--- Assignment3/Models/Entity.cs
+++ Assignment3/Models/Entity.cs
@@ -1,6 +1,20 @@
-﻿namespace Assignment3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
 {
     public class Entity
     {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            CreationTime = DateTimeOffset.Now;
+        }
     }
 }
