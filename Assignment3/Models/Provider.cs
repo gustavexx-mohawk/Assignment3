201c67a43c91379a16e03e0010678f1c1aee84a4
MARC-HI\marcanog <gustavo-marcano-valero@mohawkcollege.ca>
2023-03-04 11:17:28 -0500

    Created Provider model

---
+++ Assignment3/Models/Provider.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Provider
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+        [Required, StringLength(128)]
+        public string FirstName { get; set; }
+        [Required, StringLength(128)]
+        public string LastName { get; set; }
+        [Required]
+        public int LicenseNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
