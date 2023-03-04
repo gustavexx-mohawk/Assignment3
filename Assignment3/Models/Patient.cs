36bd3ed60db736513108ccc982f48ba95538d534
Samuel Kim <ghassad87@gmail.com>
2023-03-04 17:35:14 -0500

    Modified. Inherited to Person class

--- Assignment3/Models/Patient.cs
+++ Assignment3/Models/Patient.cs
@@ -2,18 +2,13 @@
 
 namespace Assignment3.Models
 {
-    public class Patient
+    public class Patient:Person
     {
-
-        [Key]
-        public Guid Id { get; set; }
-        [Required]
-        public DateTimeOffset CreationTime { get; set; }
-        [Required, StringLength(128)]
-        public string FirstName { get; set; }
-        [Required, StringLength(128)]
-        public string LastName { get; set; }
         [Required]
         public DateTimeOffset DateOfBirth { get; set; }
+
+        public DateTimeOffset ExpirationDate { get; set; }
+
+        public DateTimeOffset UpdatedTime { get; set; }
     }
 }
