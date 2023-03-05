5548c5dd0d706cac37a2d4bd41e3d982007cbca8
Samuel Kim <ghassad87@gmail.com>
2023-03-05 17:25:50 -0500

    Edit constructor

--- Assignment3/Models/Patient.cs
+++ Assignment3/Models/Patient.cs
@@ -2,12 +2,12 @@
 
 namespace Assignment3.Models
 {
-    public class Patient:Person
+    public class Patient : Person
     {
         [Required]
         public DateTimeOffset DateOfBirth { get; set; }        
         
-        public Patient(int year, int month, int day)
+        public Patient(int year, int month, int day) : base()
         {
             DateOfBirth = new DateTimeOffset(year, month, day, 0, 0, 0, TimeSpan.Zero);
         }
