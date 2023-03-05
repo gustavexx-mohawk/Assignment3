0f4ee2d92155ec10a1177ffb38fbd07eb69eb9d3
Samuel Kim <ghassad87@gmail.com>
2023-03-05 16:55:08 -0500

    Create constructor with params for DOB

--- Assignment3/Models/Patient.cs
+++ Assignment3/Models/Patient.cs
@@ -5,10 +5,11 @@ namespace Assignment3.Models
     public class Patient:Person
     {
         [Required]
-        public DateTimeOffset DateOfBirth { get; set; }
-
-        public DateTimeOffset ExpirationDate { get; set; }
-
-        public DateTimeOffset UpdatedTime { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }        
+        
+        public Patient(int year, int month, int day)
+        {
+            DateOfBirth = new DateTimeOffset(year, month, day, 0, 0, 0, TimeSpan.Zero);
+        }
     }
 }
