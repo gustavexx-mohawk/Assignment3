effdce759986c864edfb5138cfc08d92bc4a677c
MARC-HI\marcanog <gustavo-marcano-valero@mohawkcollege.ca>
2023-03-05 22:19:23 -0500

    Updating Immunization Model

--- Assignment3/Models/Immunization.cs
+++ Assignment3/Models/Immunization.cs
@@ -2,20 +2,21 @@
 
 namespace Assignment3.Models
 {
-    public class Immunization
+    public class Immunization : Entity
     {
-        [Key]
-        public Guid Id { get; set; }
-        [Required]
-        public DateTimeOffset CreationTime { get; set; }
         [Required, StringLength(128)]
         public string OfficialName { get; set; }
+
         [StringLength(128)]
-        public string TradeName { get; set; }
+        public string? TradeName { get; set; }
+
         [Required, StringLength(255)]
         public string LotNumber { get; set; }
+
         [Required]
         public DateTimeOffset ExpirationDate { get; set; }
-        public DateTimeOffset UpdatedTime { get; set; }
+
+        public DateTimeOffset? UpdatedTime { get { return UpdatedTime; } set { UpdatedTime = DateTimeOffset.Now; } }
+
     }
 }
