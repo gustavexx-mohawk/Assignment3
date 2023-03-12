8ad738fdb8f99f1fcea3feb44f0bb4997ce9c56a
Amanda Venier <amandav6527@gmail.com>
2023-03-12 14:30:09 -0400

    Created UnitTests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+using Assignment3.Controllers;
+using Assignment3.Data;
+using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests
+
+    // https://dotnetthoughts.net/how-to-mock-dbcontext-for-unit-testing/
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static Guid organizationId;
+        private async Task<Assignment3Context> GetAssignment3Context()
+        {
+            var options = new DbContextOptionsBuilder<Assignment3Context>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var assignment3Context = new Assignment3Context(options);
+            assignment3Context.Database.EnsureCreated();
+            if (await assignment3Context.Organization.CountAsync() <= 0)
+            {
+                Organization organization = new Organization();
+                organization.Name = "Rexall";
+                organization.Type = "Pharmacy";
+                organization.Address = "930 Upper Paradise Rd #13, Hamilton, ON L9B 2N1";
+
+                organizationId = organization.Id;
+
+                assignment3Context.Organization.Add(organization);
+
+                await assignment3Context.SaveChangesAsync();
+            }
+
+            return assignment3Context;
+        }
+
+        [TestMethod]
+        public async Task GetOrganization_ValidId()
+        {
+            // Arrange
+            var dbContext = await GetAssignment3Context();
+            
+            using var organizationsController = new OrganizationsController(dbContext);
+
+            // Act
+            ActionResult<Organization> organization = await organizationsController.GetOrganization(organizationId);
+            
+            //Assert
+            Assert.IsNotNull(organization.Value);
+        }
+    }
+}
