dbd2fb3364a2b69a93c91b366aefce12f188f9de
Piper Sicard <piper.sicard@mohawkcollege.ca>
2023-03-12 21:33:12 -0400

    Fixed Provider header comment, added comments to Person

--- Assignment3/Models/Provider.cs
+++ Assignment3/Models/Provider.cs
@@ -3,7 +3,7 @@
   Student numbers: 000826393, 000812644, 000824338, 000764961
   Date: March 12, 2023
 
-  Purpose: To represent an organization in a health care server application.
+  Purpose: To represent a provider in a health care server application.
 
   Statement of Authorship: We, Jongeun Kim (000826393), Gustavo Marcano Valero (000812644), Piper Sicard (000824338), and Amanda Venier (000764961) certify that this material is our original work.
                            No other person's work has been used without due acknowledgement.
